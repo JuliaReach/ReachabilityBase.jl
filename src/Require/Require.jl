a79a4863d392a753d08148df93441ee17dd59513
schillic <git@christianschilling.net>
2022-08-26 11:07:31 +0200

    remove require method with Main again

--- src/Require/Require.jl
+++ src/Require/Require.jl
@@ -9,15 +9,14 @@ module Require
 export require
 
 """
-    require([mod]=Main, packages; [fun_name]::String="", [explanation]::String="")
+    require(mod, packages; [fun_name]::String="", [explanation]::String="")
 
 Check for one or more optional packages and print an error message if any of
 them is not loaded.
 
 ### Input
 
-- `mod`         -- (optional; default: `Main`) module where the package should
-                   be loaded
+- `mod`         -- module where the package should be loaded; use `@__MODULE__`
 - `packages`    -- symbol or list of symbols (the package name(s))
 - `fun_name`    -- (optional; default: `""`) name of the function that requires
                    the package
@@ -29,17 +28,17 @@ them is not loaded.
 If all packages are loaded, this function has no effect.
 Otherwise it prints an error message.
 
+### Notes
+
+The argument `mod` should always be `@__MODULE__`, but since this is a macro, it
+has to be inserted by the caller.
+
 ### Algorithm
 
 This function uses `@assert` and hence loses its ability to print an error
 message if assertions are deactivated (see the `Assertions` module).
 """
 function require end
-
-# default: use Main module
-function require(package; fun_name::String="", explanation::String="")
-    require(Main, package; fun_name=fun_name, explanation=explanation)
-end
 
 # version for one package
 function require(mod, package::Symbol; fun_name::String="",
