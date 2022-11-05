8b571d56a57ae7a2e1cc20e413788c8c07908007
schillic <git@christianschilling.net>
2022-11-05 22:13:34 +0100

    adapt VectorIterator

--- src/Iteration/VectorIterator.jl
+++ src/Iteration/VectorIterator.jl
@@ -12,11 +12,11 @@ function Base.eltype(::Type{VectorIterator{T}}) where {T}
     return eltype(T)
 end
 
-function Base.iterate(it::VectorIterator, state=1)
-    if state > length(it)
+function Base.iterate(it::VectorIterator, state=0)
+    if state == length(it)
         return nothing
     end
-    element = it.vector[state]
     state += 1
+    element = it.vector[state]
     return (element, state)
 end
