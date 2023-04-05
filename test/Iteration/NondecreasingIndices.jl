63a0b49131a3c3508b42f660ece8a81fe8a934ab
schillic <git@christianschilling.net>
2023-04-05 15:18:10 +0200

    fix NondecreasingIndices with n == 1

--- test/Iteration/NondecreasingIndices.jl
+++ test/Iteration/NondecreasingIndices.jl
@@ -1,7 +1,17 @@
 using ReachabilityBase.Iteration
 
 vectors = Vector{AbstractVector{Int}}()
-for v in NondecreasingIndices(2, 3)
+ndi = NondecreasingIndices(2, 3)
+@test length(ndi) == 4
+for v in ndi
     push!(vectors, copy(v))
 end
 @test vectors == [[1, 1, 1], [1, 1, 2], [1, 2, 2], [2, 2, 2]]
+
+vectors = Vector{AbstractVector{Int}}()
+ndi = NondecreasingIndices(1, 3)
+@test length(ndi) == 1
+for v in ndi
+    push!(vectors, copy(v))
+end
+@test vectors == [[1, 1, 1]]
