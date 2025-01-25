121db16833d9f7c73a3380ef47b3c9f01b92a7ca
schillic <9656686+schillic@users.noreply.github.com>
2025-01-25 00:36:22 +0000

    Format .jl files

--- test/Arrays/matrix_operations.jl
+++ test/Arrays/matrix_operations.jl
@@ -1,4 +1,4 @@
 using StaticArrays: SVector, SMatrix
 
-P = projection_matrix([1, 2], 5, SVector{3, Int})
-@test P == [1 0 0 0 0; 0 1 0 0 0] && P isa SMatrix{2, 5, Int, 10}
+P = projection_matrix([1, 2], 5, SVector{3,Int})
+@test P == [1 0 0 0 0; 0 1 0 0 0] && P isa SMatrix{2,5,Int,10}
